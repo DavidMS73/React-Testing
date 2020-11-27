6c430842dec2d61219a323323be7463bb7b19aa6
amsuarezp <amsuarezp@gmail.com>
2020-11-26 19:14:44 -0500

    checkbox

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Checkbox from "./components/checkbox" 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Checkbox labelActive="Active" labelInactive="Inactive" />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
