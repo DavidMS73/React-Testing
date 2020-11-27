ab39a3421f4a682931fdfbd34e95ef704158169e
amsuarezp <amsuarezp@gmail.com>
2020-11-26 19:39:49 -0500

    like component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Checkbox from "./components/checkbox" 
+import Like from "./components/like"
 
 ReactDOM.render(
-  <Checkbox labelActive="Active" labelInactive="Inactive" />,
-  document.getElementById("root")
+  <Like/>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
